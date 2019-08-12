2a72aca816aba87424df58bc161babdf2275dfdb
SHI YI <shiyi2008@gmail.com>
2019-08-12 14:58:18 +0800

    1.1

--- VoxelNet.py
+++ VoxelNet.py
@@ -173,10 +173,12 @@ class VoxelNet(nn.Module):
     def voxel_indexing(self, sparse_features, coords):
         dim = sparse_features.shape[-1]
         print("sf:",sparse_features.data.shape())
+        print("dim:",dim)
         dense_feature = Variable(torch.zeros(dim, conf_dict['batch_size'], D, H, W).cuda())
         print("coords:",coords.data.shape())
+        print("dense:",dense_featue.shape())
         dense_feature[:, coords[:,0], coords[:,1], coords[:,2], coords[:,3]]= sparse_features.transpose(1, 0)
-        return dense_feature.transpose(0, 1)
+        return dense_feature#.transpose(0, 1)
 
     def forward(self, voxel_features, voxel_coords):
         # feature learning network
