b6e7c63718c83580b7ee7e59975c6813ef8a3cdc
SHI YI <shiyi2008@gmail.com>
2019-08-21 00:52:58 +0800

    d

--- inference.py
+++ inference.py
@@ -111,7 +111,7 @@ def inference(setting="val"):#test,val
         p_pos = F.sigmoid(psm.permute(0,2,3,1))#([batch, 200, 176, 2])
         p_pos = p_pos.view(-1,1).detach().cpu().numpy()
         
-        p_index = p_pos.argsort(dim=1)[::-1][:200]
+        p_index = p_pos.argsort()[::-1][:200]
         print(p_index)
         p = p_pos[p_index]
         rm = anchors_center_to_corner(rm_pos[p_index])
