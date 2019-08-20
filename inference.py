6216824bed3f2112880df8b831b943b9caa761df
SHI YI <shiyi2008@gmail.com>
2019-08-21 03:07:09 +0800

    d

--- inference.py
+++ inference.py
@@ -127,9 +127,9 @@ def inference(setting="val"):#test,val
         print(np.shape(bboxes_final))
         print(bboxes_final)
         print(gt_box3d)
-        print("oi",ids[0])
-        filr_name = ids[0].split['/'][-1].split['.'][0]
-        log_file = open("/home/screentest/ys3237/VoxelNet-pytorch/predicts/"+setting+'_'+filr_name+'txt','w')
+        
+        file_name = ids[0].split('/')[-1].split('.')[0]
+        log_file = open("/home/screentest/ys3237/VoxelNet-pytorch/predicts/"+setting+'_'+file_name+'txt','w')
         for i in bboxes_final:
             print(','.join(i)+'\n')
             log_file.write(','.join(i)+'\n')
